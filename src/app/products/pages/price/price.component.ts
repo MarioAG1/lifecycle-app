7a2189b915a5e0802abda863ae5fbc95797878c9
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-29 13:19:32 +0200

    Inicio del Proyecto - LifeCycle

---
+++ src/app/products/pages/price/price.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-price-page',
+  templateUrl: './price.component.html',
+  styleUrls: ['./price.component.css']
+})
+export class PriceComponent {
+
+}
---
+++ src/app/products/pages/product/product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-product-page',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+
+}
